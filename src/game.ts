e0c3270b1c51cbdb5d01369956ab2ca673ec70f2
nearnshaw <nearnshaw@decentraland.org>
2021-02-10 19:16:50 -0300

    update lib version

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,133 @@
-import resources from './resources'
-import { Stone, stones, seqNumbers } from './stones'
-import { getStones } from './serverHandler'
+const resources = {
+  models: {
+    base: new GLTFShape('models/baseSceneTwoByOne.glb'),
+    pool: new GLTFShape('models/pool.glb'),
+    stone: new GLTFShape('models/stone.glb'),
+    linearButton: new GLTFShape('models/linear.glb'),
+    randomButton: new GLTFShape('models/random.glb'),
+    speedButton: new GLTFShape('models/speedButton.glb'),
 
-export const sceneMessageBus = new MessageBus()
+    tube: new GLTFShape('models/tube.glb'),
+  },
+  sounds: {
+    kalimbaNotes: {
+      f3: new AudioClip('sounds/kalimba/f3.mp3'),
+      a3: new AudioClip('sounds/kalimba/a3.mp3'),
+      c3: new AudioClip('sounds/kalimba/c3.mp3'),
+      a4: new AudioClip('sounds/kalimba/a4.mp3'),
+      e4: new AudioClip('sounds/kalimba/e4.mp3'),
+      f4: new AudioClip('sounds/kalimba/f4.mp3'),
+      g4: new AudioClip('sounds/kalimba/g4.mp3'),
+    },
+  },
+}
+
+const sceneMessageBus = new MessageBus()
+
+// reusable stone class
+class MusicalDrop extends Entity {
+  note: number
+  shape: GLTFShape
+  anim: AnimationState
+
+  constructor(
+    shape: GLTFShape,
+    transform: Transform,
+
+    sound: AudioClip,
+    note: number
+  ) {
+    super()
+    engine.addEntity(this)
+    //this.addComponent(shape)
+    this.addComponent(transform)
+
+    // note ID
+    this.note = note
+
+    // store shape to add to entity later
+    this.shape = shape
+
+    // Sound
+    this.addComponent(new AudioSource(sound))
+
+    this.anim = new AnimationState('ArmatureAction.001', { looping: false })
+
+    this.addComponent(new Animator()).addClip(this.anim)
+
+    // needed to reference the entity from inside a component, because `this` in there refers to the component
+    let thisStone = this
+  }
+  public play(): void {
+    this.getComponent(AudioSource).playOnce()
+    this.anim.stop()
+    this.anim.play()
+
+    // animate
+  }
+}
+
+let drops: MusicalDrop[] = []
+
+// lightweight storage of sequencer state
+let seqNumbers: number[][] = []
+
+// reusable stone class
+class Stone extends Entity {
+  sound: AudioClip
+  index: number
+  stoneOn: boolean = false
+  drop: MusicalDrop
+  constructor(
+    shape: GLTFShape,
+    transform: Transform,
+    sound: AudioClip,
+    index: number
+  ) {
+    super()
+    engine.addEntity(this)
+    this.addComponent(shape)
+    this.addComponent(transform)
+
+    // note ID
+    this.sound = sound
+
+    this.index = index
+
+    let thisStone = this
+
+    this.addComponent(
+      new OnPointerDown(
+        (e) => {
+          log('toggle stone')
+          if (this.stoneOn) {
+            sceneMessageBus.emit('hideStone', { stone: thisStone.index })
+          } else {
+            sceneMessageBus.emit('showStone', { stone: thisStone.index })
+          }
+        },
+        {
+          button: ActionButton.POINTER,
+          hoverText: 'Toggle',
+        }
+      )
+    )
+
+    this.drop = new MusicalDrop(
+      new GLTFShape('models/music-drop.glb'),
+      new Transform({
+        position: new Vector3(0, 0, 0),
+      }),
+      this.sound,
+      this.index
+    )
+    this.drop.setParent(this)
+    this.drop.removeComponent(GLTFShape)
+    drops.push(this.drop)
+  }
+}
+
+let stones: Stone[] = []
 
 // Base scene
 const base = new Entity()
@@ -29,7 +154,7 @@ let seqOffset = new Vector3(5, 0.3, 4)
 let seqLength = 16
 
 // Kalimba sounds
-export const kalimbaSounds: AudioClip[] = [
+const kalimbaSounds: AudioClip[] = [
   resources.sounds.kalimbaNotes.f3,
   resources.sounds.kalimbaNotes.a3,
   resources.sounds.kalimbaNotes.c3,
@@ -90,3 +215,390 @@ async function updateStones() {
     }
   }
 }
+
+const loopDuration = 60
+
+// pick up message bus events, from you and other players
+sceneMessageBus.on('playStone', (e) => {
+  drops[e.note].play()
+})
+
+sceneMessageBus.on('seqOn', (e) => {
+  loopPlayer.playingMode = 1
+  loopPlayer.currentBeat = -1
+  loopPlayer.durationLeft = loopDuration
+  linear.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+  random.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+  neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+  energyAnimation.stop()
+  energyAnimation.play()
+})
+
+sceneMessageBus.on('randomMode', (e) => {
+  loopPlayer.playingMode = 2
+  loopPlayer.currentBeat = -1
+  loopPlayer.durationLeft = loopDuration
+  random.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+  linear.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+  neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+  energyAnimation.stop()
+  energyAnimation.play()
+})
+
+sceneMessageBus.on('seqOff', (e) => {
+  loopPlayer.playingMode = 0
+  linear.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+  random.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+  neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+})
+
+// system to play the loop continuously
+class PlaySequence implements ISystem {
+  playingMode: number // 0 = off, 1 = loop, 2 = random
+  currentBeat: number
+  loopDuration: number
+  durationLeft: number
+  beats: number
+  currentLoop: number
+  beatDuration: number
+  constructor(loopDuration: number, totalDuration: number, beats: number) {
+    this.durationLeft = totalDuration
+    this.loopDuration = loopDuration
+    this.beats = beats
+    this.currentLoop = 0
+    this.currentBeat = 0
+    this.beatDuration = this.loopDuration / this.beats
+  }
+  update(dt: number) {
+    if (!this.playingMode) {
+      return
+    }
+
+    this.durationLeft -= dt
+    if (this.durationLeft < 0) {
+      this.playingMode = 0
+      energyAnimation.stop()
+      linear.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+      random.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+    }
+    this.currentLoop += dt
+    if (this.currentLoop >= this.currentBeat * this.beatDuration) {
+      this.currentBeat += 1
+      if (this.currentBeat >= this.beats) {
+        this.currentLoop = 0
+        this.currentBeat = 0
+
+        log('new loop')
+      }
+      if (this.playingMode == 1) {
+        // sequence mode
+        for (let i = 0; i < 7; i++) {
+          if (seqNumbers[this.currentBeat][i]) {
+            stones[this.currentBeat * 7 + i].drop.play()
+          }
+        }
+      } else {
+        // random mode
+        let randomBeat = Math.floor(Math.random() * this.beats)
+        for (let i = 0; i < 7; i++) {
+          if (seqNumbers[randomBeat][i]) {
+            stones[randomBeat * 7 + i].drop.play()
+          }
+        }
+      }
+    }
+  }
+}
+
+///// Buttons
+let tube = new Entity()
+tube.addComponent(
+  new Transform({
+    position: new Vector3(8, 0, 11.5),
+    rotation: Quaternion.Euler(0, 270, 0),
+  })
+)
+let energyAnimation = new AnimationState('Energy_Action', { looping: false })
+tube.addComponent(new Animator()).addClip(energyAnimation)
+tube.addComponent(resources.models.tube)
+engine.addEntity(tube)
+
+let linear = new Entity()
+linear.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.48, 4.59),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+linear.addComponent(resources.models.linearButton)
+linear.setParent(tube)
+engine.addEntity(linear)
+linear.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('seqOn', {})
+    },
+    { hoverText: 'Loop' }
+  )
+)
+
+let random = new Entity()
+random.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.49, 4.33),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+random.addComponent(resources.models.randomButton)
+engine.addEntity(random)
+random.setParent(tube)
+random.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('randomMode', {})
+    },
+    { hoverText: 'Random' }
+  )
+)
+
+let slow2 = new Entity()
+slow2.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.6, 4.59),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+slow2.addComponent(resources.models.speedButton)
+slow2.setParent(tube)
+engine.addEntity(slow2)
+slow2.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('seqSpeed', { speed: 12 })
+    },
+    { hoverText: 'Very Slow' }
+  )
+)
+
+let slow1 = new Entity()
+slow1.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.6, 4.53),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+slow1.addComponent(resources.models.speedButton)
+slow1.setParent(tube)
+engine.addEntity(slow1)
+slow1.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('seqSpeed', { speed: 8 })
+    },
+    { hoverText: 'Slow' }
+  )
+)
+
+let neutral = new Entity()
+neutral.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.6, 4.47),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+neutral.addComponent(resources.models.speedButton)
+neutral.setParent(tube)
+engine.addEntity(neutral)
+neutral.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('seqSpeed', { speed: 4 })
+    },
+    { hoverText: 'Normal' }
+  )
+)
+
+let fast1 = new Entity()
+fast1.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.6, 4.41),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+fast1.addComponent(resources.models.speedButton)
+fast1.setParent(tube)
+engine.addEntity(fast1)
+fast1.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('seqSpeed', { speed: 2 })
+    },
+    { hoverText: 'Fast' }
+  )
+)
+
+let fast2 = new Entity()
+fast2.addComponent(
+  new Transform({
+    position: new Vector3(-9.54, 1.6, 4.35),
+    rotation: Quaternion.Euler(0, 180, 0),
+  })
+)
+fast2.addComponent(resources.models.speedButton)
+fast2.setParent(tube)
+engine.addEntity(fast2)
+fast2.addComponent(
+  new OnPointerDown(
+    () => {
+      sceneMessageBus.emit('seqSpeed', { speed: 1 })
+    },
+    { hoverText: 'Very Fast' }
+  )
+)
+
+sceneMessageBus.on('seqSpeed', (e) => {
+  if (loopPlayer.playingMode) {
+    let newSpeed = e.speed * 4
+
+    log('new duration = ', newSpeed)
+    loopPlayer.loopDuration = newSpeed
+    loopPlayer.beatDuration = loopPlayer.loopDuration / loopPlayer.beats
+    loopPlayer.currentLoop = loopPlayer.currentBeat * loopPlayer.beatDuration
+
+    switch (e.speed) {
+      case 12:
+        slow2.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        slow1.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast1.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        fast2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        break
+      case 8:
+        slow2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        slow1.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast1.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        fast2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        break
+      case 4:
+        slow2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        slow1.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast1.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        fast2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        break
+      case 2:
+        slow2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        slow1.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast1.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        break
+      case 1:
+        slow2.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        slow1.getComponent(Transform).rotation = Quaternion.Euler(0, 180, 0)
+        neutral.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast1.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        fast2.getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+        break
+    }
+  } else {
+    sceneMessageBus.emit('seqOn', {})
+  }
+})
+
+// start loop, w 8 second loops and with 16 beats
+let loopPlayer = new PlaySequence(8, loopDuration, 16)
+engine.addSystem(loopPlayer)
+
+// we're hardcoding the player's realm, bc it can't be fetched while the scene is being loaded. Needs to be triggered after that.
+let playerRealm = 'localhost-stub' //getRealm()
+
+// fetch the player's realm
+async function setRealm() {
+  //   let realm = await getCurrentRealm()
+  //   log(`You are in the realm: ${JSON.stringify(realm.displayName)}`)
+  playerRealm = `test`
+}
+
+// fetch the player's realm
+async function getRealm() {
+  return `test`
+}
+
+// external servers being used by the project - Please change these to your own if working on something else!
+let awsServer = 'https://genesis-plaza.s3.us-east-2.amazonaws.com/'
+let fireBaseServer = 'https://us-central1-genesis-plaza.cloudfunctions.net/app/'
+
+// get latest sequencer state stored in server
+async function getStones(): Promise<number[][]> {
+  try {
+    // if (!playerRealm) {
+    //   playerRealm = await getRealm()
+    // }
+    let url = awsServer + 'sequencer/' + playerRealm + '/stones.json'
+    log('url used ', url)
+    let response = await fetch(url)
+    let json = await response.json()
+    return json.stones
+  } catch {
+    log('error fetching from AWS server')
+  }
+}
+
+// change data in sequencer
+async function changeSequencer() {
+  // if (!playerRealm) {
+  //   await setRealm()
+  // }
+  //   seqChanger.addComponentOrReplace(
+  //     // Only send request if no more changes come over the next second
+  //     new utils.Delay(1000, async function () {
+  //       try {
+  //         let url = fireBaseServer + 'update-sequencer?realm=' + playerRealm
+  //         let body = JSON.stringify({ stones: seqNumbers })
+  //         let response = await fetch(url, {
+  //           method: 'POST',
+  //           headers: { 'Content-Type': 'application/json' },
+  //           body: body,
+  //         })
+  //         return response.json()
+  //       } catch {
+  //         log('error fetching from AWS server')
+  //       }
+  //     })
+  //   )
+}
+
+// dummy entity to throttle the sending of change requests
+let seqChanger = new Entity()
+engine.addEntity(seqChanger)
+
+sceneMessageBus.on('showStone', (e) => {
+  stones[e.stone].stoneOn = true
+  stones[e.stone].getComponent(Transform).rotation = Quaternion.Euler(0, 0, 0)
+
+  stones[e.stone].drop.addComponentOrReplace(stones[e.stone].drop.shape)
+
+  if (!loopPlayer.playingMode) {
+    stones[e.stone].drop.play()
+  }
+
+  let note = e.stone % 7
+  let beat = Math.floor(e.stone / 7)
+  log('beat ', beat, ' note ', note)
+  seqNumbers[beat][note] = 1
+  changeSequencer()
+})
+
+sceneMessageBus.on('hideStone', (e) => {
+  stones[e.stone].stoneOn = false
+  stones[e.stone].getComponent(Transform).rotation = Quaternion.Euler(180, 0, 0)
+
+  stones[e.stone].drop.removeComponent(GLTFShape)
+
+  let note = e.stone % 7
+  let beat = Math.floor(e.stone / 7)
+  seqNumbers[beat][note] = 0
+  changeSequencer()
+})
